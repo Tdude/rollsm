067a685181fe02fa4e9f402e8d8ac65f838daf7c
Tdude <tibbemail@gmail.com>
2024-02-23 00:16:33 +0100

    Added DocBlocks, multi user levels and a whole bunch of default stuff, like some db content and page on initiation (install)

---
+++ plugins/competitors/assets/predefined-rolls.php
@@ -0,0 +1,44 @@
+<?php
+return array(
+    array("name" => "Innaqatsineq —lying on the back.", "points" => "2"),
+    array("name" => "Palluussineq — lying on one's belly.", "points" => "2"),
+    array("name" => "Kinnguffik paarlallugu/nerfallaallugu — coming up on the other side, on one's back.", "points" => "2"),
+    array("name" => "Pakassummillugu/unermillugu — holding the paddle in the crook of your arm.", "points" => "2"),
+    array("name" => "Siukkut pallortillugu/masikkut — leaning forward, at the masik.", "points" => "2"),
+    array("name" => "Kingumut naatillugu  — (holding the paddle) pointed/touching aft.", "points" => "2"),
+    array("name" => "Aariammillugu  — touching the area between the shoulder blades.", "points" => "2"),
+    array("name" => "Kingup apummaatigut — (holding the paddle) at the stern-gunwale.", "points" => "3"),
+    array("name" => "Siukkut tunusummillugu  — forward, touching one's neck.", "points" => "4"),
+    array("name" => "Kingukkut tunusummillugu — backward, touching one's neck.", "points" => "4"),
+    array("name" => "Paatip kallua tuermillugu illuinnarmik — using only one arm, with the paddle touching the shoulder.", "points" => "3"),
+
+    array("name" => "Qiperuussineq/paatit ammorluinnnaq — sculling with paddle held vertically.", "points" => "4"),
+    array("name" => "Masikkut aalatsineq — sweeping the paddle at the masik.", "points" => "4"),
+    array("name" => "Tallit paarlatsillugit paateqarluni/masikkut — holding the paddle with arms crossed, at the masik.", "points" => "5"),
+    array("name" => "Qaannap ataatigut ipilaarlugu — rotating (the paddle) under the kayak", "points" => "5"),
+
+    array("name" => "Pallortillugu assakaaneq 5 — forward rolling, round and round", "points" => ""),
+    array("name" => "Nerfallarlugu assakaaneq 5 — rolling, lying on one's back, round and round", "points" => ""),
+
+    array("name" => "Avataq isserfiup taqqaanut qaannap sinarsuanut qilerullugu  — with a float tied to the deckline at the isserfik.", "points" => "5"),
+    
+    array("name" => "Norsamik masikkut — with a norsaq at the masik (forward).", "points" => "6"),
+    array("name" => "Norsamik kingukkut   — with a norsaq, starting aft.", "points" => "6"),
+    array("name" => "Norsamik nerfallaallugu — with a norsaq, lying on one's back.", "points" => "6"),
+
+    array("name" => "Assammik masikkut  — using your hand, at the masik (forward).", "points" => "7"),
+    array("name" => "Assammik kingukkut  — using your hand, starting aft.", "points" => "7"),
+    array("name" => "Assammik nerfallaallugu — using your hand, lying on one's back.", "points" => "7"),
+
+    array("name" => "Assak peqillugu /Qilerlugu/poorlugu masikkut  — making a fist at the masik.", "points" => "8"),
+    array("name" => "Assak peqillugu /Qilerlugu/poorlugu kingukkut  — making a fist starting aft.", "points" => "8"),
+    array("name" => "Assak peqillugu /Qilerlugu/poorlugu nerfallaallugu  — making a fist lying on one's back.", "points" => "8"),
+
+    array("name" => "Ujaqqamik tigumisserluni masikkut — holding a stone in one's hand at the masik.", "points" => "9"),
+    array("name" => "Ujaqqamik tigumisserluni kingukkut — holding a stone starting aft.", "points" => "9"),
+    array("name" => "Ujaqqamik tigumisserluni nerfallaallugu — holding a stone lying on one's back..", "points" => "9"),
+
+    array("name" => "Ikusaannarmik niaqoq/pukusuk patillugu — only with the elbow, touching the head/ neck.", "points" => "10"),
+    array("name" => "Tallit paarlatsillugit timaannarmik — crossing the arms, only with the body.", "points" => "11"),
+    array("name" => "Pusilluni paarneq — paddling with the kayak turned upside down.", "points" => ""),
+);
