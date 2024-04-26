1e6cb024a25101bfce8629f31dae076cca115f6f
Tdude <tibbemail@gmail.com>
2024-04-26 19:40:19 +0200

    Partial cleanup and better logic for validation

--- plugins/competitors/assets/script.js
+++ plugins/competitors/assets/script.js
@@ -1,172 +1,145 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("competitors-registration-form");
-  const validationMessageContainer =
-    document.getElementById("validation-message");
-  const submitButton = document.getElementById("submit-button");
-  const licenseCheckboxDiv = document.getElementById("license-container");
-  const radioButtons = document.querySelectorAll(
+  const validationMessageContainer = form.querySelector("#validation-message");
+  const submitButton = form.querySelector("#submit-button");
+  const radioButtons = form.querySelectorAll(
     'input[type="radio"][name="participation_class"]'
   );
 
-  function showValidationMessage(message, success) {
-    validationMessageContainer.querySelector(".message-content").textContent =
-      message;
-    toggleValidationMessage(true, success);
-
-    // Apply to start fade-out effect
-    validationMessageContainer.classList.add("fade-out");
-
-    // Wait before closing
-    setTimeout(() => {
-      closeValidationMessage();
-    }, 8000); // 1000 is 1 second
+  function setupEventListeners() {
+    form.addEventListener("submit", handleSubmit);
+    form
+      .querySelectorAll('.extra-visible input[type="checkbox"]')
+      .forEach((checkbox) => {
+        checkbox.addEventListener("change", (event) =>
+          toggleClass(
+            event.target.closest(".extra-visible"),
+            "border-danger",
+            !event.target.checked
+          )
+        );
+      });
+    const radioButtons = form.querySelectorAll(
+      'input[type="radio"][name="participation_class"]'
+    );
+    radioButtons.forEach((radio) =>
+      radio.addEventListener("change", toggleLicenseCheckbox)
+    );
+    toggleLicenseCheckbox(); // Set initial state
   }
 
-  function closeValidationMessage() {
-    validationMessageContainer.classList.add("hidden");
-    validationMessageContainer.classList.remove("fade-out");
-    validationMessageContainer.querySelector(".message-content").textContent =
-      "";
-  }
-
-  function toggleValidationMessage(visible, success) {
-    validationMessageContainer.classList.toggle("hidden", !visible);
-    validationMessageContainer.classList.toggle("danger", !success);
-    validationMessageContainer.classList.toggle("success", success);
-  }
-
-  function appendValidationMessage(message) {
-    // Insert message into .message-content span
-    validationMessageContainer.querySelector(".message-content").textContent =
-      message;
-    showValidationMessage(message, false); // Pass the message and success flag
-  }
-
-  // Handle form submission
   async function handleSubmit(event) {
     event.preventDefault();
-    submitButton.disabled = true;
-    submitButton.value = "Processing...";
-    toggleValidationMessage(false);
+    showButtonLoading(submitButton, true);
 
     if (validateForm()) {
-      submitForm();
+      await submitForm();
     } else {
-      resetSubmitButton();
+      showButtonLoading(submitButton, false);
     }
   }
 
   function validateForm() {
     closeValidationMessage();
+    let isValid = true;
 
-    if (!["name", "email", "phone"].every(validateField)) return false;
-    if (!validateParticipationClass()) return false;
-    if (!validateConsent()) return false;
+    // Validate required fields
+    ["phone", "email", "name"].forEach((fieldName) => {
+      const field = form.querySelector(`[name="${fieldName}"]`);
+      if (!field.value.trim()) {
+        const capitalizedMessage =
+          fieldName.charAt(0).toUpperCase() + fieldName.slice(1);
+        displayValidationMessage(capitalizedMessage + " is required.", false);
+        toggleClass(field, "border-danger", true);
+        isValid = false;
+      } else {
+        toggleClass(field, "border-danger", false);
+      }
+    });
 
-    return true;
-  }
-
-  // Validate individual fields
-  function validateField(fieldName) {
-    const field = form.querySelector(`[name="${fieldName}"]`);
-    if (!field.value.trim()) {
-      appendValidationMessage(
-        `For registering, ${
-          fieldName.charAt(0).toUpperCase() + fieldName.slice(1)
-        } is required.`
-      );
-      field.classList.add("border-danger");
-      return false;
-    } else {
-      field.classList.remove("border-danger");
-    }
-    return true;
-  }
-
-  // Validate participation class selection and potentially the license agreement
-  function validateParticipationClass() {
-    const participationClassContainer = document.getElementById(
-      "participation-class-container"
+    // Validate participation class and consent
+    isValid &= validateRadioSection(
+      "participation_class",
+      "You are required to choose which class to participate in."
     );
-    if (!form.querySelector('input[name="participation_class"]:checked')) {
-      appendValidationMessage("Participation class choice is required.");
-      participationClassContainer.classList.add("border-danger");
-      // Focus on the first radio button for participation class
-      const firstRadioButton = participationClassContainer.querySelector(
-        'input[type="radio"]'
-      );
-      firstRadioButton.focus();
-      window.scrollTo({
-        top: firstRadioButton.offsetTop - 20,
-        behavior: "smooth",
-      });
-      return false;
-    } else {
-      participationClassContainer.classList.remove("border-danger");
-    }
-    return true;
+    isValid &= validateCheckbox("consent", "Your consent is required.");
+    toggleClass(field, "border-danger", true);
+    return isValid;
   }
 
-  // Validate consent checkbox
-  function validateConsent() {
-    const consentCheckbox = form.querySelector('[name="consent"]');
-    const consentContainer = document.getElementById("consent-container");
-    if (!consentCheckbox.checked) {
-      appendValidationMessage("Consent is required.");
-      consentContainer.classList.add("border-danger");
-      consentCheckbox.focus();
-      window.scrollTo({
-        top: consentCheckbox.offsetTop - 20,
-        behavior: "smooth",
-      });
+  function validateRadioSection(name, message) {
+    const container = form.querySelector(`#${name}-container`);
+    if (!form.querySelector(`input[name="${name}"]:checked`)) {
+      displayValidationMessage(message, false);
+      toggleClass(container, "border-danger", true);
       return false;
     } else {
-      consentContainer.classList.remove("border-danger");
+      toggleClass(container, "border-danger", false);
+      return true;
     }
-    return true;
   }
 
-  // Handle visibility and class toggling for the license agreement section
+  function validateCheckbox(name, message) {
+    const checkbox = form.querySelector(`[name="${name}"]`);
+    const container = checkbox.closest(".form-group");
+    if (!checkbox.checked) {
+      displayValidationMessage(message, false);
+      toggleClass(container, "border-danger", true);
+      return false;
+    } else {
+      toggleClass(container, "border-danger", false);
+      return true;
+    }
+  }
+
   function toggleLicenseCheckbox() {
-    const isChampionshipSelected =
-      document.getElementById("championship").checked;
-    const licenseCheckboxDiv = document.getElementById("license-container");
-    const licenseCheckbox = document.getElementById("license-check");
-    if (isChampionshipSelected) {
-      // Show the license agreement section and remove the 'hidden' and 'border-danger' classes if needed
-      toggleElementDisplay(licenseCheckboxDiv, "", 1, ["show"], ["hidden"]);
-    } else {
-      // Hide the license agreement section, add the 'border-danger' class, and remove 'show' class
-      toggleElementDisplay(
-        licenseCheckboxDiv,
-        "none",
-        0,
-        ["border-danger"],
-        ["show"]
-      );
-      // Uncheck the license agreement checkbox
-      licenseCheckbox.checked = false;
+    const championshipSelected = form.querySelector("#championship").checked;
+    const licenseContainer = form.querySelector("#license-container");
+    toggleClass(licenseContainer, "hidden", !championshipSelected);
+  }
+
+  function toggleClass(element, className, condition) {
+    if (element && condition) {
+      element.classList.add(className);
+    } else if (element) {
+      element.classList.remove(className);
     }
   }
 
-  // Function to add or remove the border-danger class based on the checkbox state
-  function toggleParentBorder() {
-    document
-      .querySelectorAll('.extra-visible input[type="checkbox"]')
-      .forEach((checkbox) => {
-        checkbox.addEventListener("change", function () {
-          const parentDiv = this.closest(".extra-visible");
-          if (this.checked) {
-            parentDiv.classList.remove("border-danger");
-          } else {
-            parentDiv.classList.add("border-danger");
-          }
-        });
-      });
+  function displayValidationMessage(message, isSuccess) {
+    validationMessageContainer.textContent = message;
+    validationMessageContainer.classList.toggle("hidden", false);
+    validationMessageContainer.classList.toggle("danger", !isSuccess);
+    validationMessageContainer.classList.toggle("success", isSuccess);
   }
-  toggleParentBorder();
 
-  // Submit form data using Fetch API
+  function handleValidationMessage({
+    message = "Ärrår",
+    success = true,
+    show = false,
+    fadeOut = true,
+  }) {
+    validationMessageContainer.querySelector(".message-content").textContent =
+      message;
+    validationMessageContainer.classList.toggle("hidden", !show);
+    validationMessageContainer.classList.toggle("danger", !success && show);
+    validationMessageContainer.classList.toggle("success", success && show);
+
+    if (fadeOut) {
+      validationMessageContainer.classList.add("fade-out");
+      setTimeout(() => closeValidationMessage(), 8000);
+    }
+  }
+
+  function closeValidationMessage() {
+    validationMessageContainer.classList.add("hidden");
+  }
+
+  function showButtonLoading(button, isLoading) {
+    button.disabled = isLoading;
+    button.value = isLoading ? "Processing..." : "Submit";
+  }
+
   async function submitForm() {
     console.log("Handling form submission async");
     const formData = new FormData(form);
@@ -181,58 +154,47 @@ document.addEventListener("DOMContentLoaded", function () {
       });
 
       if (!response.ok)
-        throw new Error("Server not reachable. Please try again later.");
+        throw new Error("Oops! Server not reachable. Please try again later.");
 
       const data = await response.json();
       if (!data.success) {
         throw new Error(`Error from server: ${data.data.message}`);
       } else {
-        toggleValidationMessage(
+        handleValidationMessage(
+          "Yay! Your submission was successful. We will stay in touch via email.",
           true,
           true,
-          "Your submission was successful! We will stay in touch via email."
+          true
         );
-        // Handle post-submission logic (e.g., clear form, redirect slug from settings)
+
         form.reset();
         setTimeout(() => {
-          // @Todo: fix this!
-          window.location.href = `${WPSettings.baseURL}/${WPSettings.thankYouSlug}`;
+          window.location.href = `${competitorsPublicAjax.baseURL}/${competitorsPublicAjax.thankYouSlug}`;
         }, 5000);
       }
     } catch (error) {
       console.error("Error during form submission:", error);
-      let errorMessage = "There was a problem with your submission."; // Default message
+      let errorMessage = "Oops! There was a problem with your submission."; // Default message
       if (error.message) {
-        errorMessage = `There was a problem with your submission: ${error.message}`;
+        errorMessage = `Oops! There was a problem with your submission: ${error.message}`;
       }
-      appendValidationMessage(errorMessage);
+      handleValidationMessage(errorMessage, false, true, true);
     } finally {
       resetSubmitButton();
     }
   }
 
-  // Reset the submit button to its initial state
   function resetSubmitButton() {
-    submitButton.disabled = false;
+    submitButton.removeAttribute("disabled");
     submitButton.value = "Submit";
-    console.log("Submit reset");
   }
+
+  setupEventListeners();
 
   // Attach the toggle function to each radio button's change event
   radioButtons.forEach(function (radioButton) {
     radioButton.addEventListener("change", toggleLicenseCheckbox);
   });
-
-  // Initialize event listeners
-  function initEventListeners() {
-    form.addEventListener("submit", handleSubmit);
-    radioButtons.forEach((button) =>
-      button.addEventListener("change", toggleLicenseCheckbox)
-    );
-    toggleLicenseCheckbox(); // Set initial state
-  }
-
-  initEventListeners();
 
   // Utility function to toggle display, opacity, and classes for elements
   function toggleElementDisplay(
@@ -258,46 +220,39 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  // Handle row clicks to toggle individual checkboxes
-  const rows = document.querySelectorAll(".clickable-row");
+  // Handle row and checkbox interactions
+  const rows = form.querySelectorAll(".clickable-row");
   rows.forEach((row) => {
     row.addEventListener("click", function (event) {
-      // Prevent toggling if the clicked element is a checkbox
-      if (event.target.type !== "checkbox") {
-        const checkbox = this.querySelector(".roll-checkbox");
-        if (checkbox) {
+      const isCheckbox = event.target.type === "checkbox";
+      const checkbox = isCheckbox
+        ? event.target
+        : this.querySelector(".roll-checkbox");
+      if (checkbox) {
+        if (!isCheckbox) {
           checkbox.checked = !checkbox.checked;
-          // Trigger the change event on the checkbox
-          checkbox.dispatchEvent(new Event("change"));
+        }
+        toggleClass(checkbox.closest("tr"), "grayed-out", !checkbox.checked);
+        // If the checkbox state was changed manually, trigger the change event
+        if (!isCheckbox) {
+          checkbox.dispatchEvent(new Event("change", { bubbles: true }));
         }
       }
     });
   });
 
-  // Master checkbox functionality
-  const masterCheckbox = document.getElementById("check_all");
+  // "Master" checkbox functionality
+  const masterCheckbox = form.querySelector("#check_all");
   masterCheckbox?.addEventListener("change", function () {
-    const checkboxes = document.querySelectorAll(
+    const checkboxes = form.querySelectorAll(
       'input[type="checkbox"].roll-checkbox'
     );
     checkboxes.forEach((checkbox) => {
       // Check or uncheck all except the master checkbox itself
       if (checkbox !== masterCheckbox) {
         checkbox.checked = masterCheckbox.checked;
-        // Trigger the change event on each checkbox to handle related changes
         checkbox.dispatchEvent(new Event("change"));
       }
-    });
-  });
-
-  // Optional: Listen to individual checkbox changes if you need to do something when they change
-  const checkboxes = document.querySelectorAll(
-    'input[type="checkbox"].roll-checkbox'
-  );
-  checkboxes.forEach((checkbox) => {
-    checkbox.addEventListener("change", function () {
-      // Implement any additional logic needed when a checkbox changes
-      // For example, updating a counter, changing styles, etc.
     });
   });
 
