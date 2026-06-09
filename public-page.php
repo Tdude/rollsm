fa51a67ac260a3a8ce8b7e197305a5885d17d1ed
Tdude <tibbemail@gmail.com>
2026-06-09 23:15:52 +0200

    chore: changed admission price for 2026

--- public-page.php
+++ public-page.php
@@ -403,7 +403,8 @@ function send_confirmation_email($name, $email, $competition_date, $total_sum, $
     $subject = 'Registration Confirmation for RollSM';
     $message = "Hej $name!\n\n" .
                 "Tack för din anmälan till RollSM $competition_date. Vi ser fram emot att få träffas!\n\n" .
-                "Din erlagda avgift som du har eller ska Swisha borde vara $total_sum.\n\n";
+                "Du kommer att få ett separat mail med mer information kring boendebokning mm.\n\n" .
+                "Din erlagda avgift som du har eller ska Swisha är $total_sum.\n\n";
 
     if ($dinner === 'yes') {
         $message .= "Middag: Ja (200 SEK)\n";
@@ -413,6 +414,7 @@ function send_confirmation_email($name, $email, $competition_date, $total_sum, $
                 "==============\n\n\n\n".
                 "Hi $name,\n\n".
                 "Thank you for registering for the RollSM competition on $competition_date. We look forward to seeing you there!\n\n".
+                "You will be receiving a separate mail with details on accommodation etc.\n\n".
                 "Your total fee is SEK $total_sum.\n\n";
 
     if ($dinner === 'yes') {
@@ -447,9 +449,9 @@ add_filter('the_content', 'competitors_thank_you_content');
 function get_competitor_price_list() {
     // Prices for each class
     $competition_prices = [
-        'amateur' => 300,
-        'championship' => 500,
-        'open' => 500,
+        'amateur' => 600,
+        'championship' => 600,
+        'open' => 600,
     ];
 
     // Dinner is optional
